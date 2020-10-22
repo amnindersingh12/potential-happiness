5fa210232278efb8b7c32cf71a06329b65691bc0
amninder <amnindersingh58611@gmail.com>
2020-10-22 14:25:45 +0530

    Add existing file

--- sketch.js
+++ sketch.js
@@ -1,23 +1,44 @@
 var song;
 var sliderRate;
 var sliderPan;
+var button;
 
 function setup() {
     createCanvas(200, 200);
     song = loadSound("onekiss.mp3", loaded);
-    song.setVolume(0.5);
+    button = createButton("play");
+    button.mousePressed(togglePlaying);
+
+
     sliderRate = createSlider(0.5, 1.5, 1, 0.01);
-    // sliderPan = createSlider(0, 1, 0.5, 0.01);
+    sliderPan = createSlider(-1, 1, 0, 0.01);
 
 }
 
+function togglePlaying() {
+    if (!song.isPlaying()) {
+
+        song.play();
+        song.setVolume(0.5);
+        button.html("pause");
+
+    } else {
+        song.pause();
+        button.html("play");
+    }
+}
+
 function loaded() {
-    song.play();
+    // song.play();
+    console.log("loaded");
 
 }
 
 function draw() {
-    background(random(25));
+    background((43));
+    //Drawing Pan and Rate slider
+    // song.html("Rate")
     song.rate(sliderRate.value());
+    song.pan(sliderPan.value());
 
 }
