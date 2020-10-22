1d6898ad5e9bed0e738923b61d8a347b01e9fff0
amninder <amnindersingh58611@gmail.com>
2020-10-22 15:45:24 +0530

    Add Play and Pause Buttton

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # potential-happiness
 creating A music player using p5.js
+- load Sound (preload or callback)
+- play() or loop() 
+- setVolume()
+- rate() and pan()
+-------------
+- jump()
+- duration() 
+- current Time()
--- sketch.js
+++ sketch.js
@@ -1,18 +1,32 @@
+const audioFile = "onekiss.mp3";
 var song;
 var sliderRate;
 var sliderPan;
 var button;
+var jumpButton;
 
 function setup() {
     createCanvas(200, 200);
-    song = loadSound("onekiss.mp3", loaded);
+    // Adding song
+    song = loadSound(audioFile, loaded);
+    // Drawing Button
     button = createButton("play");
     button.mousePressed(togglePlaying);
 
-
+    //Jump at a particular section
+    jumpButton = createButton("jump");
+    jumpButton.mousePressed(jumpSong);
+    background(51);
+    //Drawing Pan and Rate slider
     sliderRate = createSlider(0.5, 1.5, 1, 0.01);
     sliderPan = createSlider(-1, 1, 0, 0.01);
 
+}
+
+function jumpSong() {
+    var len = song.duration();
+    var t = random(len)
+    song.jump(t);
 }
 
 function togglePlaying() {
@@ -30,13 +44,13 @@ function togglePlaying() {
 
 function loaded() {
     // song.play();
-    console.log("loaded");
+    console.log("Song loaded");
 
 }
 
 function draw() {
-    background((43));
-    //Drawing Pan and Rate slider
+
+    background(song.currentTime() * 10, song.currentTime() * 15, song.currentTime() * 20);
     // song.html("Rate")
     song.rate(sliderRate.value());
     song.pan(sliderPan.value());
